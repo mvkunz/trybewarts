c37a73437cb8f6b93afbe352fb194101b49f33be
Monique Pedra <moniquepedrinha@gmail.com>
2023-05-27 17:15:23 -0300

    Correção do form requisito 3 e estilização do header e h1

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// const myEmail = document.querySelector('.trybewarts-login.email');
-// const myPassword = document.querySelector('.trybewarts-login.password');
-// const myButton = document.querySelectorAll('.submit');
-// myButton.addEventListener('click',function(){
-//     Event.preventDefault();
-//     const userEmail = myEmail.value;
-//     const userPassword = myPassword.value;
-//     if( userEmail === 'tryber@teste.com' && userPassword === '123456') {
-//        alert('Olá, tryber!')
-//     }else {
-//        alert('Email ou senha inválidos')
-//     }
-// });
+const myEmail = document.querySelector('.trybewarts-login.email');
+const myPassword = document.querySelector('.trybewarts-login.password');
+const myButtons = document.querySelectorAll('.submit');
+
+for (let index = 0; index < myButtons.length; index += 1) {
+  myButtons[index].addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const userEmail = myEmail.value;
+    const userPassword = myPassword.value;
+
+    if (userEmail === 'tryber@teste.com' && userPassword === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos');
+    }
+  });
+}
