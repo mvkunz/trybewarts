5a612d62abcd852d26ed73cc7a286226cddcb2f9
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-05-27 15:18:11 -0300

    requisito 4 ok

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-
-const myEmail = document.querySelector('.trybewarts-login.email');
-const myPassword = document.querySelector('.trybewarts-login.password');
-const myButton = document.querySelectorAll('.submit');
-myButton.addEventListener('click',function(){
-    Event.preventDefault();
-    const userEmail = myEmail.value;
-    const userPassword = myPassword.value;
-    if( userEmail === 'tryber@teste.com' && userPassword === '123456') {
-       alert('Olá, tryber!')
-    }else {
-       alert('Email ou senha inválidos')
-    }
-});
+// const myEmail = document.querySelector('.trybewarts-login.email');
+// const myPassword = document.querySelector('.trybewarts-login.password');
+// const myButton = document.querySelectorAll('.submit');
+// myButton.addEventListener('click',function(){
+//     Event.preventDefault();
+//     const userEmail = myEmail.value;
+//     const userPassword = myPassword.value;
+//     if( userEmail === 'tryber@teste.com' && userPassword === '123456') {
+//        alert('Olá, tryber!')
+//     }else {
+//        alert('Email ou senha inválidos')
+//     }
+// });
  
 
 
