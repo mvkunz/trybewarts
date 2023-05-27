0f8741cb81178d12ddb714f4d4507ffed2450666
Laura <lauravieirapires@gmail.com>
2023-05-26 21:37:43 -0300

    fazendo requisito 3

---
+++ script.js
@@ -0,0 +1,10 @@
+
+ const email = form.email.Email;
+ const password = form.password.Senha;
+ if( email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, tryber!')
+ }else {
+    alert('Email ou senha inválidos')
+ }
+
+
